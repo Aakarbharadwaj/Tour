3915ca3853439e0a20eebd44241b53c21b015fd9
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-04-09 23:27:56 +0530

    new commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Card.css'
 const Card = ({ id, name, info, image, price }) => {
+    const [desc, setDesc] = useState("false");
+    const description = `${info.substring(0, 100)}...`
 
-    const description = info.sub
     return (
         <div className='card'>
             <img className='image' src={image} />
             <div className='card-content '>
                 <h3>{price}/-</h3>
                 <h1>{name}</h1>
-                <p className='card-description'>{info}... <span>Read more</span></p>
+                <p className='card-description'>{description} <span>Read more</span></p>
             </div>
             <div className='button'>
                 <button>Not interested</button>
