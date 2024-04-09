f745e8414eb5cbcfe1f32d08b54d017f66666ceb
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-04-09 23:39:19 +0530

    new add

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,16 +2,18 @@ import React, { useState } from 'react'
 import './Card.css'
 const Card = ({ id, name, info, image, price }) => {
     const [readmore, setReadmore] = useState(false);
-    if(!desc)?
+    
     const description = `${info.substring(0, 100)}...`
-
+    const readmoreHandler = ()=>{
+        setReadmore(!readmore);
+    }
     return (
         <div className='card'>
             <img className='image' src={image} />
             <div className='card-content '>
                 <h3>{price}/-</h3>
                 <h1>{name}</h1>
-                <p className='card-description'>{description} <span>{readmore?'Show less':'Read more'}</span></p>
+                <p className='card-description'>{description} <span onClick={readmoreHandler}>{readmore?'Show less':'Read more'}</span></p>
             </div>
             <div className='button'>
                 <button>Not interested</button>
